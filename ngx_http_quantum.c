7f07eb1dcd443e6f78c65bd2b1c9590b149dce3b
tokikuch <k.toshihito@yahoo.de>
2023-09-17 21:16:25 -0700

    HTTP Handler module

--- ngx_http_quantum.c
+++ ngx_http_quantum.c
@@ -1,40 +1,42 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
+#include <ngx_http.h>
 
-typedef struct {
-  ngx_flag_t enable;
-} ngx_http_quantum_conf_t;
-
-static void *ngx_http_quantum_create_conf(ngx_cycle_t *cycle);
-static char *ngx_http_quantum_init_conf(ngx_cycle_t *cycle, void *conf);
-
-static char *ngx_http_quantum_enable(ngx_conf_t *cf, void *post, void *data);
-static ngx_conf_post_t ngx_http_quantum_enable_post = {
-  ngx_http_quantum_enable
-};
+static char* ngx_http_quantum_set(
+    ngx_conf_t* cf,
+    ngx_command_t* cmd,
+    void* conf);
 
 static ngx_command_t ngx_http_quantum_commands[] = {
-  { ngx_string("quantum_enabled"),
-    NGX_MAIN_CONF|NGX_DIRECT_CONF|NGX_CONF_FLAG,
-    ngx_conf_set_flag_slot,
+  { ngx_string("foo"),
+    NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
+    ngx_http_quantum_set,
     0,
-    offsetof(ngx_http_quantum_conf_t, enable),
-    &ngx_http_quantum_enable_post },
+    0,
+    NULL },
 
   ngx_null_command
 };
 
-static ngx_core_module_t ngx_http_quantum_module_ctx = {
-  ngx_string("quantum"),
-  ngx_http_quantum_create_conf,
-  ngx_http_quantum_init_conf,
+static ngx_http_module_t ngx_http_quantum_module_ctx = {
+  NULL, /* preconfiguration */
+  NULL, /* postconfiguration */
+
+  NULL, /* create main configuration */
+  NULL, /* init main configuration */
+
+  NULL, /* create server configuration */
+  NULL, /* merge server configuration */
+
+  NULL, /* create location configuration */
+  NULL /* merge location configuration */
 };
 
 ngx_module_t ngx_http_quantum_module = {
   NGX_MODULE_V1,
   &ngx_http_quantum_module_ctx, /* module context */
   ngx_http_quantum_commands, /* module directives */
-  NGX_CORE_MODULE, /* module type */
+  NGX_HTTP_MODULE, /* module type */
   NULL, /* init master */
   NULL, /* init module */
   NULL, /* init process */
@@ -45,29 +47,36 @@ ngx_module_t ngx_http_quantum_module = {
   NGX_MODULE_V1_PADDING
 };
 
-static void* ngx_http_quantum_create_conf(ngx_cycle_t *cycle) {
-  ngx_http_quantum_conf_t *fcf =
-    ngx_pcalloc(cycle->pool, sizeof(ngx_http_quantum_conf_t));
-  if (fcf == NULL) {
-    return NULL;
+static ngx_str_t ngx_http_json_type = ngx_string("application/json");
+static ngx_str_t ngx_return_json =
+  ngx_string("{\"message\": \"\xF0\x9F\x98\x8E\"}");
+
+static ngx_int_t ngx_http_quantum_handler(ngx_http_request_t* r) {
+  ngx_table_elt_t* ua = r->headers_in.user_agent;
+  if (ua == NULL) {
+    return NGX_DECLINED;
   }
 
-  fcf->enable = NGX_CONF_UNSET;
-  return fcf;
-}
-
-static char* ngx_http_quantum_init_conf(ngx_cycle_t* cycle, void* conf) {
-  ngx_http_quantum_conf_t* fcf = conf;
-  ngx_conf_init_value(fcf->enable, 0);
-  return NGX_CONF_OK;
-}
-
-static char* ngx_http_quantum_enable(ngx_conf_t* cf, void* post, void* data) {
-  ngx_flag_t* fp = data;
-  if (*fp == 0) {
-    return NGX_CONF_OK;
+  if (ua->value.len == 3 && ngx_strncmp(ua->value.data, "foo", 3) == 0) {
+    ngx_log_error(NGX_LOG_NOTICE, r->connection->log, 0, "Rejecting foo");
+    return NGX_HTTP_FORBIDDEN;
   }
 
-  ngx_log_error(NGX_LOG_NOTICE, cf->log, 0, "Quantum Module is enabled");
+  ngx_http_complex_value_t cv;
+  ngx_memzero(&cv, sizeof(ngx_http_complex_value_t));
+  cv.value = ngx_return_json;
+
+  return ngx_http_send_response(r, NGX_HTTP_OK, &ngx_http_json_type, &cv);
+}
+
+static char* ngx_http_quantum_set(
+    ngx_conf_t *cf,
+    ngx_command_t* cmd,
+    void* conf) {
+  ngx_http_core_loc_conf_t* clcf =
+    ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+  clcf->handler = ngx_http_quantum_handler;
+
+  ngx_log_error(NGX_LOG_NOTICE, cf->log, 0, "Set ngx_http_quantum_handler");
   return NGX_CONF_OK;
 }
